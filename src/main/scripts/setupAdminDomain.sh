40656e3ebe929f785a953b9e7682fed7bdfe06b8
Galia <haixia.cheng@mircosoft.com>
2020-05-08 16:10:29 +0800

    Task 1117339: Do initial R&D in admin offer

--- src/main/scripts/setupAdminDomain.sh
+++ src/main/scripts/setupAdminDomain.sh
@@ -9,23 +9,7 @@ function echo_stderr ()
 #Function to display usage message
 function usage()
 {
-  echo_stderr "./setupAdminDomain.sh <acceptOTNLicenseAgreement> <otnusername> <otnpassword> <wlsDomainName> <wlsUserName> <wlsPassword> <wlsAdminHost>"  
-}
-
-function setupInstallPath()
-{
-    JDK_PATH="/u01/app/jdk"
-    WLS_PATH="/u01/app/wls"
-    DOMAIN_PATH="/u01/domains"
-    WL_HOME="/u01/app/wls/install/Oracle/Middleware/Oracle_Home/wlserver"
-
-    #create custom directory for setting up wls and jdk
-    sudo mkdir -p $JDK_PATH
-    sudo mkdir -p $WLS_PATH
-    sudo mkdir -p $DOMAIN_PATH
-    sudo rm -rf $JDK_PATH/*
-    sudo rm -rf $WLS_PATH/*
-    sudo rm -rf $DOMAIN_PATH/*
+  echo_stderr "./setupAdminDomain.sh <wlsDomainName> <wlsUserName> <wlsPassword> <wlsAdminHost> <oracleHome>"  
 }
 
 function installUtilities()
@@ -50,52 +34,6 @@ function installUtilities()
     done  
 }
 
-function addOracleGroupAndUser()
-{
-    #add oracle group and user
-    echo "Adding oracle user and group..."
-    groupname="oracle"
-    username="oracle"
-    user_home_dir="/u01/oracle"
-    USER_GROUP=${groupname}
-    sudo groupadd $groupname
-    sudo useradd -d ${user_home_dir} -g $groupname $username
-}
-
-#download jdk from OTN
-function downloadJDK()
-{
-   echo "Downloading jdk from OTN..."
-
-   for in in {1..5}
-   do
-     ${SCRIPT_PWD}/oradown.sh --cookie=accept-weblogicserver-server --username="${otnusername}" --password="${otnpassword}" https://download.oracle.com/otn/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
-     tar -tzf jdk-8u131-linux-x64.tar.gz 
-     if [ $? != 0 ];
-     then
-        echo "Download failed. Trying again..."
-        rm -f jdk-8u131-linux-x64.tar.gz
-     else 
-        echo "Downloaded JDK successfully"
-        break
-     fi
-   done
-}
-
-#download 3rd Party JDBC Drivers
-function downloadJDBCDrivers()
-{
-   echo "Downloading JDBC Drivers..."
-      
-   echo "Downloading postgresql Driver..."
-   downloadUsingWget ${POSTGRESQL_JDBC_DRIVER_URL}
-   
-   echo "Downloading mssql Driver"
-   downloadUsingWget ${MSSQL_JDBC_DRIVER_URL}
-   
-   echo "JDBC Drivers Downloaded Completed Successfully."
-}
-
 function downloadUsingWget()
 {
    downloadURL=$1
@@ -114,126 +52,10 @@ function downloadUsingWget()
    done
 }
 
-function copyJDBCDriversToWeblogicClassPath()
-{
-     echo "Copying JDBC Drivers to Weblogic CLASSPATH ..."
-     sudo cp $BASE_DIR/${POSTGRESQL_JDBC_DRIVER} ${WL_HOME}/server/lib/
-     sudo cp $BASE_DIR/${MSSQL_JDBC_DRIVER} ${WL_HOME}/server/lib/
-     
-     chown $username:$groupname ${WL_HOME}/server/lib/${POSTGRESQL_JDBC_DRIVER}
-     chown $username:$groupname ${WL_HOME}/server/lib/${MSSQL_JDBC_DRIVER}
-     
-     echo "Copied JDBC Drivers to Weblogic CLASSPATH"
-}
-
-function modifyWLSClasspath()
-{
-  echo "Modify WLS CLASSPATH ...."
-  sed -i 's;^WEBLOGIC_CLASSPATH=\"${JAVA_HOME}.*;&\nWEBLOGIC_CLASSPATH="${WL_HOME}/server/lib/postgresql-42.2.8.jar:${WL_HOME}/server/lib/mssql-jdbc-7.4.1.jre8.jar:${WEBLOGIC_CLASSPATH}";' ${WL_HOME}/../oracle_common/common/bin/commExtEnv.sh
-  sed -i 's;^WEBLOGIC_CLASSPATH=\"${JAVA_HOME}.*;&\n\n#**WLSAZURECUSTOMSCRIPTEXTENSION** Including Postgresql and MSSSQL JDBC Drivers in Weblogic Classpath;' ${WL_HOME}/../oracle_common/common/bin/commExtEnv.sh
-  echo "Modified WLS CLASSPATH."
-}
-
-function setupJDK()
-{
-    sudo cp $BASE_DIR/jdk-8u131-linux-x64.tar.gz $JDK_PATH/jdk-8u131-linux-x64.tar.gz
-
-    echo "extracting and setting up jdk..."
-    sudo tar -zxvf $JDK_PATH/jdk-8u131-linux-x64.tar.gz --directory $JDK_PATH
-    sudo chown -R $username:$groupname $JDK_PATH
-
-    export JAVA_HOME=$JDK_PATH/jdk1.8.0_131
-    export PATH=$JAVA_HOME/bin:$PATH
-
-    java -version
-
-    if [ $? == 0 ];
-    then
-        echo "JAVA HOME set succesfully."
-    else
-        echo_stderr "Failed to set JAVA_HOME. Please check logs and re-run the setup"
-        exit 1
-    fi
-}
-
-function setupWLS()
-{
-    sudo cp $BASE_DIR/fmw_12.2.1.3.0_wls_Disk1_1of1.zip $WLS_PATH/fmw_12.2.1.3.0_wls_Disk1_1of1.zip
-    echo "unzipping fmw_12.2.1.3.0_wls_Disk1_1of1.zip..."
-    sudo unzip -o $WLS_PATH/fmw_12.2.1.3.0_wls_Disk1_1of1.zip -d $WLS_PATH
-
-    export SILENT_FILES_DIR=$WLS_PATH/silent-template
-    sudo mkdir -p $SILENT_FILES_DIR
-    sudo rm -rf $WLS_PATH/silent-template/*
-    sudo chown -R $username:$groupname $WLS_PATH
-
-    export INSTALL_PATH="$WLS_PATH/install"
-    export WLS_JAR="$WLS_PATH/fmw_12.2.1.3.0_wls.jar"
-
-    mkdir -p $INSTALL_PATH
-    sudo chown -R $username:$groupname $INSTALL_PATH
-
-    create_oraInstlocTemplate
-    create_oraResponseTemplate
-    create_oraUninstallResponseTemplate
-
-}
-
-#Download Weblogic install jar from OTN
-function downloadWLS()
-{
-  echo "Downloading weblogic install kit from OTN..."
-
-  for in in {1..5}
-  do
-     ${SCRIPT_PWD}/oradown.sh --cookie=accept-weblogicserver-server --username="${otnusername}" --password="${otnpassword}" http://download.oracle.com/otn/nt/middleware/12c/12213/fmw_12.2.1.3.0_wls_Disk1_1of1.zip
-     unzip -l fmw_12.2.1.3.0_wls_Disk1_1of1.zip
-     if [ $? != 0 ];
-     then
-        echo "Download failed. Trying again..."
-        rm -f fmw_12.2.1.3.0_wls_Disk1_1of1.zip
-     else 
-        echo "Downloaded WLS successfully"
-        break
-     fi
-  done
-}
-
-
-function validateJDKZipCheckSum()
-{
-  jdkZipFile="$BASE_DIR/jdk-8u131-linux-x64.tar.gz"
-  jdk18u131Sha256Checksum="62b215bdfb48bace523723cdbb2157c665e6a25429c73828a32f00e587301236"
-
-  downloadedJDKZipCheckSum=$(sha256sum $jdkZipFile | cut -d ' ' -f 1)
-
-  if [ "${jdk18u131Sha256Checksum}" == "${downloadedJDKZipCheckSum}" ];
-  then
-    echo "Checksum match successful. Proceeding with Weblogic Install Kit Zip Download from OTN..."
-  else
-    echo "Checksum match failed. Please check the supplied OTN credentials and try again."
-    exit 1
-  fi
-}
-
-
 #Function to cleanup all temporary files
 function cleanup()
 {
     echo "Cleaning up temporary files..."
-	
-    rm -f $BASE_DIR/jdk-8u131-linux-x64.tar.gz
-    rm -f $BASE_DIR/fmw_12.2.1.3.0_wls_Disk1_1of1.zip
-	
-    rm -rf $JDK_PATH/jdk-8u131-linux-x64.tar.gz
-    rm -rf $WLS_PATH/fmw_12.2.1.3.0_wls_Disk1_1of1.zip
-    
-    rm -rf $BASE_DIR/${POSTGRESQL_JDBC_DRIVER}
-    rm -rf $BASE_DIR/${MSSQL_JDBC_DRIVER}
-    
-    rm -rf $WLS_PATH/silent-template
-    	
-    rm -rf $WLS_JAR
 
     rm -rf $DOMAIN_PATH/admin-domain.yaml
     rm -rf $DOMAIN_PATH/weblogic-deploy.zip
@@ -242,114 +64,6 @@ function cleanup()
     rm -rf $DOMAIN_PATH/shoppingcart.zip
  
     echo "Cleanup completed."
-}
-
-#Function to create Weblogic Installation Location Template File for Silent Installation
-function create_oraInstlocTemplate()
-{
-    echo "creating Install Location Template..."
-
-    cat <<EOF >$WLS_PATH/silent-template/oraInst.loc.template
-inventory_loc=[INSTALL_PATH]
-inst_group=[GROUP]
-EOF
-}
-
-#Function to create Weblogic Installation Response Template File for Silent Installation
-function create_oraResponseTemplate()
-{
-
-    echo "creating Response Template..."
-
-    cat <<EOF >$WLS_PATH/silent-template/response.template
-[ENGINE]
-
-#DO NOT CHANGE THIS.
-Response File Version=1.0.0.0.0
-
-[GENERIC]
-
-#Set this to true if you wish to skip software updates
-DECLINE_AUTO_UPDATES=false
-
-#My Oracle Support User Name
-MOS_USERNAME=
-
-#My Oracle Support Password
-MOS_PASSWORD=<SECURE VALUE>
-
-#If the Software updates are already downloaded and available on your local system, then specify the path to the directory where these patches are available and set SPECIFY_DOWNLOAD_LOCATION to true
-AUTO_UPDATES_LOCATION=
-
-#Proxy Server Name to connect to My Oracle Support
-SOFTWARE_UPDATES_PROXY_SERVER=
-
-#Proxy Server Port
-SOFTWARE_UPDATES_PROXY_PORT=
-
-#Proxy Server Username
-SOFTWARE_UPDATES_PROXY_USER=
-
-#Proxy Server Password
-SOFTWARE_UPDATES_PROXY_PASSWORD=<SECURE VALUE>
-
-#The oracle home location. This can be an existing Oracle Home or a new Oracle Home
-ORACLE_HOME=[INSTALL_PATH]/Oracle/Middleware/Oracle_Home
-
-#Set this variable value to the Installation Type selected. e.g. WebLogic Server, Coherence, Complete with Examples.
-INSTALL_TYPE=WebLogic Server
-
-#Provide the My Oracle Support Username. If you wish to ignore Oracle Configuration Manager configuration provide empty string for user name.
-MYORACLESUPPORT_USERNAME=
-
-#Provide the My Oracle Support Password
-MYORACLESUPPORT_PASSWORD=<SECURE VALUE>
-
-#Set this to true if you wish to decline the security updates. Setting this to true and providing empty string for My Oracle Support username will ignore the Oracle Configuration Manager configuration
-DECLINE_SECURITY_UPDATES=true
-
-#Set this to true if My Oracle Support Password is specified
-SECURITY_UPDATES_VIA_MYORACLESUPPORT=false
-
-#Provide the Proxy Host
-PROXY_HOST=
-
-#Provide the Proxy Port
-PROXY_PORT=
-
-#Provide the Proxy Username
-PROXY_USER=
-
-#Provide the Proxy Password
-PROXY_PWD=<SECURE VALUE>
-
-#Type String (URL format) Indicates the OCM Repeater URL which should be of the format [scheme[Http/Https]]://[repeater host]:[repeater port]
-COLLECTOR_SUPPORTHUB_URL=
-
-
-EOF
-}
-
-#Function to create Weblogic Uninstallation Response Template File for Silent Uninstallation
-function create_oraUninstallResponseTemplate()
-{
-    echo "creating Uninstall Response Template..."
-
-    cat <<EOF >$WLS_PATH/silent-template/uninstall-response.template
-[ENGINE]
-
-#DO NOT CHANGE THIS.
-Response File Version=1.0.0.0.0
-
-[GENERIC]
-
-#This will be blank when there is nothing to be de-installed in distribution level
-SELECTED_DISTRIBUTION=WebLogic Server~[WLSVER]
-
-#The oracle home location. This can be an existing Oracle Home or a new Oracle Home
-ORACLE_HOME=[INSTALL_PATH]/Oracle/Middleware/Oracle_Home/
-
-EOF
 }
 
 #Creates weblogic deployment model for admin domain
@@ -381,8 +95,8 @@ function create_app_deploy_model()
     echo "Creating deploying applicaton model"
     cat <<EOF >$DOMAIN_PATH/deploy-app.yaml
 domainInfo:
-   AdminUserName: $wlsUserName
-   AdminPassword: $wlsPassword
+   AdminUserName: "$wlsUserName"
+   AdminPassword: "$wlsPassword"
    ServerStartMode: prod
 appDeployments:
    Application:
@@ -399,6 +113,11 @@ function create_adminDomain()
     echo "Creating Admin Only Domain"
     echo "Creating domain path /u01/domains"
     echo "Downloading weblogic-deploy-tool"
+
+    DOMAIN_PATH="/u01/domains"
+    sudo mkdir -p $DOMAIN_PATH
+    sudo rm -rf $DOMAIN_PATH/*
+
     cd $DOMAIN_PATH
     wget -q $WEBLOGIC_DEPLOY_TOOL  
     if [[ $? != 0 ]]; then
@@ -408,7 +127,7 @@ function create_adminDomain()
     sudo unzip -o weblogic-deploy.zip -d $DOMAIN_PATH
     create_admin_model
     sudo chown -R $username:$groupname $DOMAIN_PATH
-    runuser -l oracle -c "export JAVA_HOME=$JDK_PATH/jdk1.8.0_131 ; $DOMAIN_PATH/weblogic-deploy/bin/createDomain.sh -oracle_home $INSTALL_PATH/Oracle/Middleware/Oracle_Home -domain_parent $DOMAIN_PATH  -domain_type WLS -model_file $DOMAIN_PATH/admin-domain.yaml" 
+    runuser -l oracle -c ". $oracleHome/oracle_common/common/bin/setWlstEnv.sh; $DOMAIN_PATH/weblogic-deploy/bin/createDomain.sh -oracle_home $oracleHome -domain_parent $DOMAIN_PATH  -domain_type WLS -model_file $DOMAIN_PATH/admin-domain.yaml"
     if [[ $? != 0 ]]; then
        echo "Error : Domain creation failed"
        exit 1
@@ -486,76 +205,16 @@ function deploy_sampleApp()
 	echo "Downloading and Deploying Sample Application"
 	wget -q $samplApp
 	sudo unzip -o shoppingcart.zip -d $DOMAIN_PATH
-	sudo chown -R $username:$groupname $DOMAIN_PATH/shoppingcart.*
-	runuser -l oracle -c "export JAVA_HOME=$JDK_PATH/jdk1.8.0_131 ;  $DOMAIN_PATH/weblogic-deploy/bin/deployApps.sh -oracle_home $INSTALL_PATH/Oracle/Middleware/Oracle_Home -archive_file $DOMAIN_PATH/shoppingcart.war -domain_home $DOMAIN_PATH/$wlsDomainName -model_file  $DOMAIN_PATH/deploy-app.yaml"
+    sudo chown -R $username:$groupname $DOMAIN_PATH/shoppingcart.*
+    runuser -l oracle -c ". $oracleHome/oracle_common/common/bin/setWlstEnv.sh; $DOMAIN_PATH/weblogic-deploy/bin/deployApps.sh -oracle_home $oracleHome -archive_file $DOMAIN_PATH/shoppingcart.war -domain_home $DOMAIN_PATH/$wlsDomainName -model_file  $DOMAIN_PATH/deploy-app.yaml"
 	if [[ $? != 0 ]]; then
        echo "Error : Deploying application failed"
        exit 1
     fi
 }
 
-#Install Weblogic Server using Silent Installation Templates
-function installWLS()
-{
-    # Using silent file templates create silent installation required files
-    echo "Creating silent files for installation from silent file templates..."
-
-    sed 's@\[INSTALL_PATH\]@'"$INSTALL_PATH"'@' ${SILENT_FILES_DIR}/uninstall-response.template > ${SILENT_FILES_DIR}/uninstall-response
-    sed -i 's@\[WLSVER\]@'"$WLS_VER"'@' ${SILENT_FILES_DIR}/uninstall-response
-    sed 's@\[INSTALL_PATH\]@'"$INSTALL_PATH"'@' ${SILENT_FILES_DIR}/response.template > ${SILENT_FILES_DIR}/response
-    sed 's@\[INSTALL_PATH\]@'"$INSTALL_PATH"'@' ${SILENT_FILES_DIR}/oraInst.loc.template > ${SILENT_FILES_DIR}/oraInst.loc
-    sed -i 's@\[GROUP\]@'"$USER_GROUP"'@' ${SILENT_FILES_DIR}/oraInst.loc
-
-    echo "Created files required for silent installation at $SILENT_FILES_DIR"
-
-    export UNINSTALL_SCRIPT=$INSTALL_PATH/Oracle/Middleware/Oracle_Home/oui/bin/deinstall.sh
-    if [ -f "$UNINSTALL_SCRIPT" ]
-    then
-            currentVer=`. $INSTALL_PATH/Oracle/Middleware/Oracle_Home/wlserver/server/bin/setWLSEnv.sh 1>&2 ; java weblogic.version |head -2`
-            echo "#########################################################################################################"
-            echo "Uninstalling already installed version :"$currentVer
-            runuser -l oracle -c "$UNINSTALL_SCRIPT -silent -responseFile ${SILENT_FILES_DIR}/uninstall-response"
-            sudo rm -rf $INSTALL_PATH/*
-            echo "#########################################################################################################"
-    fi
-
-    echo "---------------- Installing WLS ${WLS_JAR} ----------------"
-    echo $JAVA_HOME/bin/java -d64 -jar  ${WLS_JAR} -silent -invPtrLoc ${SILENT_FILES_DIR}/oraInst.loc -responseFile ${SILENT_FILES_DIR}/response -novalidation
-    runuser -l oracle -c "$JAVA_HOME/bin/java -d64 -jar  ${WLS_JAR} -silent -invPtrLoc ${SILENT_FILES_DIR}/oraInst.loc -responseFile ${SILENT_FILES_DIR}/response -novalidation"
-
-    # Check for successful installation and version requested
-    if [[ $? == 0 ]];
-    then
-      echo "Weblogic Server Installation is successful"
-    else
-
-      echo_stderr "Installation is not successful"
-      exit 1
-    fi
-    echo "#########################################################################################################"
-
-}
-
-
 function validateInput()
 {
-    if [ -z "$acceptOTNLicenseAgreement" ];
-    then
-            echo _stderr "acceptOTNLicenseAgreement is required. Value should be either Y/y or N/n"
-            exit 1
-    fi
-    if [[ ! ${acceptOTNLicenseAgreement} =~ ^[Yy]$ ]];
-    then
-        echo "acceptOTNLicenseAgreement value not specified as Y/y (yes). Exiting installation Weblogic Server process."
-        exit 1
-    fi
-
-    if [[ -z "$otnusername" || -z "$otnpassword" ]]
-    then
-        echo_stderr "otnusername or otnpassword is required. "
-        exit 1
-    fi	
-
     if [ -z "$wlsDomainName" ];
     then
         echo_stderr "wlsDomainName is required. "
@@ -573,6 +232,12 @@ function validateInput()
         echo_stderr "wlsAdminHost is required. "
         exit 1
     fi
+
+    if [ -z "$oracleHome" ];
+    then
+        echo_stderr "oracleHome is required. "
+        exit 1
+    fi
 }
 
 function enableAndStartAdminServerService()
@@ -588,63 +253,33 @@ function enableAndStartAdminServerService()
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BASE_DIR="$(readlink -f ${CURR_DIR})"
 
-if [ $# -ne 7 ]
+if [ $# -ne 5 ]
 then
     usage
 	exit 1
 fi
 
-export acceptOTNLicenseAgreement="$1"
-export otnusername="$2"
-export otnpassword="$3"
-export wlsDomainName="$4"
-export wlsUserName="$5"
-export wlsPassword="$6"
-export wlsAdminHost="$7"
+export wlsDomainName="$1"
+export wlsUserName="$2"
+export wlsPassword="$3"
+export wlsAdminHost="$4"
+export oracleHome="$5"
 
 validateInput
 
 installUtilities
 
-export WLS_VER="12.2.1.3.0"
-export WEBLOGIC_DEPLOY_TOOL=https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.1.1/weblogic-deploy.zip
+export WEBLOGIC_DEPLOY_TOOL=https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.8.1/weblogic-deploy.zip
 export samplApp="https://www.oracle.com/webfolder/technetwork/tutorials/obe/fmw/wls/10g/r3/cluster/session_state/files/shoppingcart.zip"
 export wlsAdminPort=7001
 export wlsSSLAdminPort=7002
 export wlsAdminURL="$wlsAdminHost:$wlsAdminPort"
-
-export POSTGRESQL_JDBC_DRIVER_URL=https://jdbc.postgresql.org/download/postgresql-42.2.8.jar 
-export POSTGRESQL_JDBC_DRIVER=${POSTGRESQL_JDBC_DRIVER_URL##*/}
-
-export MSSQL_JDBC_DRIVER_URL=https://repo.maven.apache.org/maven2/com/microsoft/sqlserver/mssql-jdbc/7.4.1.jre8/mssql-jdbc-7.4.1.jre8.jar
-export MSSQL_JDBC_DRIVER=${MSSQL_JDBC_DRIVER_URL##*/}
+export username="oracle"
+export groupname="oracle"
 
 export SCRIPT_PWD=`pwd`
-chmod ugo+x ${SCRIPT_PWD}/oradown.sh 
 
-addOracleGroupAndUser
-
-cleanup
-
-setupInstallPath
-
-downloadJDK
-
-validateJDKZipCheckSum 
-
-downloadWLS
-
-setupJDK
-
-setupWLS
-
-installWLS
-
-downloadJDBCDrivers
-
-copyJDBCDriversToWeblogicClassPath
-
-modifyWLSClasspath
+setWlsEnv
 
 create_adminDomain
 
