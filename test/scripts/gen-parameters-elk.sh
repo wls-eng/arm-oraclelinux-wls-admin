11de38f4729809cdfd9d2d23d5e241e71c91cec0
haixia <haixia.cheng@microsoft.com>
2020-09-16 17:18:53 +0800

    CI/CD for ELK
    Fix script format.

---
+++ test/scripts/gen-parameters-elk.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+parametersPath=$1
+githubUserName=$2
+testbranchName=$3
+
+cat <<EOF >${parametersPath}
+{
+    "\$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "_artifactsLocation": {
+            "value": "https://raw.githubusercontent.com/${githubUserName}/arm-oraclelinux-wls-admin/${testbranchName}/src/main/arm/"
+        },
+        "_artifactsLocationSasToken": {
+            "value": ""
+        },
+        "adminPasswordOrKey": {
+            "value": "GEN-UNIQUE"
+        },
+        "adminUsername": {
+            "value": "GEN-UNIQUE"
+        },
+        "elasticsearchPassword": {
+            "value": "GEN-UNIQUE"
+        },
+        "elasticsearchPort": {
+            "value": "GEN-UNIQUE"
+        },
+        "elasticsearchURI": {
+            "value": "GEN-UNIQUE"
+        },
+        "elasticsearchUserName": {
+            "value": "GEN-UNIQUE"
+        },
+        "enableAAD": {
+            "value": false
+        },
+        "enableDB": {
+            "value": false
+        },
+        "enableELK": {
+            "value": true
+        },
+        "wlsPassword": {
+            "value": "GEN-UNIQUE"
+        },
+        "wlsUserName": {
+            "value": "GEN-UNIQUE"
+        }
+    }
+}
+EOF
